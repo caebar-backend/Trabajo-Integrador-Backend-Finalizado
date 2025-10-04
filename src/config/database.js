0a572a99ec8a9715af8caf5693c116ddb1b25f8a
[caebar] <[caebar.cursos.programacion@gmail.com]>
2025-10-04 16:29:36 -0300

    Commit 3: Archivo database.js concretado

--- src/config/database.js
+++ src/config/database.js
@@ -3,5 +3,29 @@
  * Los estudiantes deben completar la configuración de la conexión
  */
 
-const sequelize = require('sequelize')
+const { Sequelize } = require('sequelize')
+
 process.loadEnvFile()
+
+const dbHost = process.env.DB_HOST
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const dataBase = process.env.DB_NAME
+const dbPort = process.env.DB_PORT
+
+const sequelizeConfig = new Sequelize(
+    dataBase,
+    dbUser,
+    dbPassword,
+    {
+        host: dbHost,
+        port: dbPort,
+        dialect: 'mysql',
+    })
+
+ module.exports = sequelizeConfig
+
+ 
+
+
+
